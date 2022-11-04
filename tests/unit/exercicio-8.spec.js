4152505f9ba0db84b23037284458cdd83998b95e
Breno-MT <hentgenp@gmail.com>
2022-11-04 19:17:52 -0300

    [BRENO] Done exercicio 8

--- tests/unit/exercicio-8.spec.js
+++ tests/unit/exercicio-8.spec.js
@@ -7,4 +7,42 @@ describe("ExercicioOito.vue", () => {
 
     await wrapper.find("button.previous").trigger("click");
   });
+
+  it("clica no botão pra avançar", async () => {
+    const wrapper = shallowMount(ExercicioOito);
+
+    await wrapper.find("button.next").trigger("click");
+
+    expect(wrapper.text()).toMatch("Anterior Página atual: 2 Próxima");
+  });
+
+  it("clica no botão até chegar na página 5", async () => {
+    const wrapper = shallowMount(ExercicioOito);
+
+    await wrapper.find("button.next").trigger("click");
+    await wrapper.find("button.next").trigger("click");
+    await wrapper.find("button.next").trigger("click");
+    await wrapper.find("button.next").trigger("click");
+
+    expect(wrapper.text()).toMatch("Anterior Página atual: 5 Próxima")
+  });
+  
+  it("clica no botão até chegar na página 4 e diminuir uma página", async () => {
+    const wrapper = shallowMount(ExercicioOito);
+
+    await wrapper.find("button.next").trigger("click");
+    await wrapper.find("button.next").trigger("click");
+    await wrapper.find("button.next").trigger("click");
+    await wrapper.find("button.previous").trigger("click");
+
+    expect(wrapper.text()).toMatch("Anterior Página atual: 3 Próxima")
+  });
+  
+  it("clica no botão previous e não pode ser menor que página 1", async () => {
+    const wrapper = shallowMount(ExercicioOito);
+
+    await wrapper.find("button.previous").trigger("click");
+
+    expect(wrapper.text()).toMatch("Anterior Página atual: 1 Próxima")
+  });
 });
